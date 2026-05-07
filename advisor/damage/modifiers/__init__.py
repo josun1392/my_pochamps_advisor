ec9fe4315c628086ecb39535625654ad9c60c2a2
Gawon <33649@naver.com>
2026-05-07 12:34:35 +0900

    refactor(3.7-pre): convert damage modifiers module to package

---
+++ advisor/damage/modifiers/__init__.py
@@ -0,0 +1,23 @@
+from advisor.damage.modifiers.core import (
+    TransformState,
+    calc_stab,
+    sand_spdef_boost,
+    snow_def_boost,
+    stab_modifier,
+    terrain_attack_modifier,
+    terrain_defense_modifier,
+    type_effectiveness_with_field,
+    weather_modifier,
+)
+
+__all__ = [
+    "TransformState",
+    "calc_stab",
+    "sand_spdef_boost",
+    "snow_def_boost",
+    "stab_modifier",
+    "terrain_attack_modifier",
+    "terrain_defense_modifier",
+    "type_effectiveness_with_field",
+    "weather_modifier",
+]
